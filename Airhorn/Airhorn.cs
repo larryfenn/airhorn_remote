d8587e89c2735d666d6cea46d0ed1327810488ae
larry <larry@larryfenn.com>
2016-11-04 21:21:05 -0400

    Initial commit.

---
+++ Airhorn/Airhorn.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net.Sockets;
+
+namespace Airhorn {
+  public partial class Airhorn : Form {
+    public Airhorn() {
+      InitializeComponent();
+      TcpListener listener = new TcpListener(42069); // nice
+      listener.Start();
+      while (true) {
+        Socket client = listener.AcceptSocket();
+        Console.WriteLine("Received");
+        SendKeys.SendWait("A");
+        client.Close();
+      }
+    }
+  }
+}
---
+++ Airhorn/Start.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Airhorn {
+  static class Start {
+    [STAThread]
+    static void Main() {
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new Airhorn());
+    }
+  }
+}
