2ec3602956e40e588d17132732f5f0000247ac49
Larry Fenn <larry.fenn@gmail.com>
2017-01-18 23:49:46 -0500

    Uses SoundPlayer to properly playback sample

--- Airhorn/Airhorn.cs
+++ Airhorn/Airhorn.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Globalization;
 using System.IO;
+using System.Media;
 using System.Windows.Forms;
 using System.Net;
 using System.Net.Sockets;
@@ -8,12 +9,14 @@ using System.Net.Sockets;
 namespace Airhorn {
   public partial class Airhorn : Form {
     public Airhorn() {
-      InitializeComponent();
+      InitializeComponent(); // probably re-do this not as a Windows Forms thing - that's what this does
+      SoundPlayer airhorn = new SoundPlayer(@".\airhorn.wav"); // can I link this to the project somehow?
+      airhorn.Load();
       TcpListener listener = new TcpListener(42069); // nice
       listener.Start();
       while (true) {
-        TcpClient client = listener.AcceptTcpClient();
-        SendKeys.SendWait("S");
+        TcpClient client = listener.AcceptTcpClient(); // program will stick here until a request is received
+        airhorn.Play();
         String sender = ((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString();
         using (System.IO.StreamWriter file =
           new System.IO.StreamWriter(@"C:\Users\Larry Fenn\Desktop\Airhorn\airhornlog.txt", true))
